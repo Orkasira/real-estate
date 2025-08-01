21f96429da3fcc0942b7e80d632829275493f956
Dachi Kekenadze <dachikekenadze5@gmail.com>
2025-08-01 23:24:06 +0400

    list design finished

--- src/Addlist/Addlist.jsx
+++ src/Addlist/Addlist.jsx
@@ -2,20 +2,43 @@ import "./Addlist.css";
 import { instance } from "../Main/FIlter/FIlter";
 import React, { useState, useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
+import trash from "../assets/trash-2.png";
+import plusCircle from "../assets/plus-circle.png";
+import { Link } from "react-router-dom";
 
 function Addlist() {
   const [regions, setRegions] = useState([]);
   const [cities, setCities] = useState([]);
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const agentBtnRef = useRef(null);
+  const removeBtnRef = useRef(null);
+  const blurOverlayRef = useRef(null);
+  const [activeInput, setActiveInput] = useState(null);
 
+  // agent form validation
   const {
     register,
     handleSubmit,
     watch,
     trigger,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      Email: "",
+      telNum: "",
+    },
+  });
 
   const onSubmit = async (data) => console.log(data);
+
+  const firstNameValue = watch("firstName");
+  const lastNameValue = watch("lastName");
+  const EmailValue = watch("Email");
+  const telNumValue = watch("telNum");
 
   // city and region lists
 
@@ -67,6 +90,36 @@ function Addlist() {
     (city) => String(city.region_id) === String(selectedRegionId)
   );
 
+  // image add and remove
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage(file);
+      setImageUrl(URL.createObjectURL(file));
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setImage(null);
+    setImageUrl("");
+  };
+
+  // agent add and remove
+  function agent() {
+    removeBtnRef.current.style.display = "block";
+    blurOverlayRef.current.style.display = "block";
+  }
+
+  function remove() {
+    removeBtnRef.current.style.display = "none";
+    blurOverlayRef.current.style.display = "none";
+  }
+
+  function overlayRemove() {
+    removeBtnRef.current.style.display = "none";
+    blurOverlayRef.current.style.display = "none";
+  }
+
   return (
     <>
       <div className="add-list-container">
@@ -89,12 +142,12 @@ function Addlist() {
             <h3>მდებარეობა</h3>
             <div className="placement-content">
               <div className="placement-item">
-                <label htmlFor="">მისამართი *</label>
-                <input type="text" />
+                <label htmlFor="misamarti">მისამართი *</label>
+                <input type="text" id="misamarti" />
               </div>
               <div className="placement-item">
-                <label htmlFor="">საფოსტო ინდექსი *</label>
-                <input type="text" />
+                <label htmlFor="index">საფოსტო ინდექსი *</label>
+                <input type="text" id="index" />
               </div>
             </div>
             <div className="region-city-container">
@@ -127,7 +180,353 @@ function Addlist() {
               </div>
             </div>
           </div>
+          <div className="detail-container">
+            <h3>ბინის დეტალები</h3>
+            <div className="detail-content">
+              <div className="detail-item">
+                <label htmlFor="price">ფასი</label>
+                <input type="text" id="price" />
+              </div>
+              <div className="detail-item">
+                <label htmlFor="m2">ფართობი</label>
+                <input type="text" id="m2" />
+              </div>
+            </div>
+            <div className="detail-item">
+              <label htmlFor="bedrooms">საძინებლების რაოდენობა*</label>
+              <input type="text" id="bedrooms" />
+            </div>
+            <div className="description">
+              <label htmlFor="">აღწერა *</label>
+              <textarea name="" id="" className="text"></textarea>
+            </div>
+            <div className="photos">
+              <label
+                htmlFor="file-upload"
+                style={{ color: imageUrl ? "green" : "initial" }}
+              >
+                ატვირთეთ ფოტო *
+              </label>
+              {!imageUrl ? (
+                <label className="custom-file-upload" htmlFor="file-upload">
+                  <span className="plus">+</span>
+                  <input
+                    id="file-upload"
+                    type="file"
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    onChange={handleFileChange}
+                  />
+                </label>
+              ) : (
+                <div
+                  className="custom-file-upload"
+                  style={{
+                    border: imageUrl
+                      ? "2px dashed green"
+                      : "2px dashed #021526",
+                  }}
+                >
+                  <div className="image-preview-wrapper">
+                    <img
+                      src={imageUrl}
+                      alt="preview"
+                      className="image-preview"
+                    />
+                    <button
+                      type="button"
+                      className="remove-image-btn"
+                      onClick={handleRemoveImage}
+                    >
+                      <img src={trash} alt="trash" className="trash" />
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="agent-container">
+            <h3>აგენტი</h3>
+            <p>აირჩიე</p>
+            <div className="agent-select-box">
+              <select>
+                <option value="">აირჩიე</option>
+              </select>
+            </div>
+            <div className="add-agent-btn">
+              <button type="button" ref={agentBtnRef} onClick={agent}>
+                <img src={plusCircle} alt="" /> დაამატე აგენტი
+              </button>
+              <div
+                className="blur-overlay"
+                ref={blurOverlayRef}
+                onClick={overlayRemove}
+              ></div>
+              <div className="add-agent" ref={removeBtnRef}>
+                <h2 className="title">აგენტის დამატება</h2>
+                <form className="form" onSubmit={handleSubmit(onSubmit)}>
+                  <div className="fullname">
+                    <div className="inputs">
+                      <label htmlFor="">სახელი *</label>
+                      <input
+                        type="text"
+                        id="firstName"
+                        placeholder="სახელი"
+                        {...register("firstName", {
+                          required: "✓ მინიმუმ ორი სიმბოლო",
+                          minLength: {
+                            value: 2,
+                            message: "✓ ჩაწერეთ ვალიდური მონაცემები",
+                          },
+                        })}
+                        onFocus={() => setActiveInput("firstName")}
+                        onBlur={() => setActiveInput(null)}
+                        style={{
+                          border:
+                            firstNameValue.length === 0
+                              ? "1.5px solid #021526"
+                              : errors.firstName
+                              ? "1.5px solid #F93B1D"
+                              : "1.5px solid green",
+                          outline:
+                            activeInput === "firstName"
+                              ? firstNameValue.length === 0
+                                ? "1.5px solid #021526"
+                                : errors.firstName
+                                ? "1.5px solid #F93B1D"
+                                : "1.5px solid green"
+                              : "none",
+                          padding: "2px",
+                        }}
+                      />
+
+                      {firstNameValue.length === 0 ? (
+                        <p style={{ color: "#021526" }}>
+                          ✓ მინიმუმ ორი სიმბოლო
+                        </p>
+                      ) : errors.firstName ? (
+                        <p style={{ color: "#F93B1D" }}>
+                          ✓ ჩაწერეთ ვალიდური მონაცემები
+                        </p>
+                      ) : (
+                        <p style={{ color: "green" }}>✓ მინიმუმ ორი სიმბოლო</p>
+                      )}
+                    </div>
+                    <div className="inputs">
+                      <label htmlFor="">გვარი</label>
+                      <input
+                        type="text"
+                        id="lastName"
+                        placeholder="სახელი"
+                        {...register("lastName", {
+                          required: "✓ მინიმუმ ორი სიმბოლო",
+                          minLength: {
+                            value: 2,
+                            message: "✓ ჩაწერეთ ვალიდური მონაცემები",
+                          },
+                        })}
+                        onFocus={() => setActiveInput("lastName")}
+                        onBlur={() => setActiveInput(null)}
+                        style={{
+                          border:
+                            lastNameValue.length === 0
+                              ? "1.5px solid #021526"
+                              : errors.lastName
+                              ? "1.5px solid #F93B1D"
+                              : "1.5px solid green",
+                          outline:
+                            activeInput === "lastName"
+                              ? lastNameValue.length === 0
+                                ? "1.5px solid #021526"
+                                : errors.lastName
+                                ? "1.5px solid #F93B1D"
+                                : "1.5px solid green"
+                              : "none",
+                          padding: "2px",
+                        }}
+                      />
+
+                      {lastNameValue.length === 0 ? (
+                        <p style={{ color: "#021526" }}>
+                          ✓ მინიმუმ ორი სიმბოლო
+                        </p>
+                      ) : errors.lastName ? (
+                        <p style={{ color: "#F93B1D" }}>
+                          ✓ ჩაწერეთ ვალიდური მონაცემები
+                        </p>
+                      ) : (
+                        <p style={{ color: "green" }}>✓ მინიმუმ ორი სიმბოლო</p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="mail-tel">
+                    <div className="inputs">
+                      <label htmlFor="">ელ-ფოსტა*</label>
+                      <input
+                        type="email"
+                        id="Email"
+                        placeholder="ელ-ფოსტა"
+                        {...register("Email", {
+                          required: "✓ გამოიყენეთ @redberry.ge ფოსტა",
+                          validate: (value) =>
+                            value.endsWith("@redberry.ge") ||
+                            "✓ ჩაწერეთ ვალიდური მონაცემები",
+                        })}
+                        onFocus={() => setActiveInput("Email")}
+                        onBlur={() => setActiveInput(null)}
+                        style={{
+                          border:
+                            EmailValue.length === 0
+                              ? "1.5px solid #021526"
+                              : errors.Email
+                              ? "1.5px solid #F93B1D"
+                              : "1.5px solid green",
+                          outline:
+                            activeInput === "Email"
+                              ? EmailValue.length === 0
+                                ? "1.5px solid #021526"
+                                : errors.Email
+                                ? "1.5px solid #F93B1D"
+                                : "1.5px solid green"
+                              : "none",
+                          padding: "2px",
+                        }}
+                      />
+
+                      {EmailValue.length === 0 ? (
+                        <p style={{ color: "#021526" }}>
+                          ✓ გამოიყენეთ @redberry.ge ფოსტა
+                        </p>
+                      ) : errors.Email ? (
+                        <p style={{ color: "#F93B1D" }}>
+                          ✓ ჩაწერეთ ვალიდური მონაცემები
+                        </p>
+                      ) : (
+                        <p style={{ color: "green" }}>
+                          ✓ გამოიყენეთ @redberry.ge ფოსტა
+                        </p>
+                      )}
+                    </div>
+                    <div className="inputs">
+                      <label htmlFor="">ტელეფონის ნომერი</label>
+                      <input
+                        type="text"
+                        id="telNum"
+                        placeholder="ტელეფონის ნომერი"
+                        {...register("telNum", {
+                          required: "✓ მხოლოდ 9 რიცხვი დაიწყეთ 5 ით",
+                          pattern: {
+                            value: /^5\d{8}$/,
+                            message: "✓ ჩაწერეთ ვალიდური მონაცემები",
+                          },
+                        })}
+                        onFocus={() => setActiveInput("telNum")}
+                        onBlur={() => setActiveInput(null)}
+                        style={{
+                          border:
+                            telNumValue.length === 0
+                              ? "1.5px solid #021526"
+                              : errors.telNum
+                              ? "1.5px solid #F93B1D"
+                              : "1.5px solid green",
+                          outline:
+                            activeInput === "telNum"
+                              ? telNumValue.length === 0
+                                ? "1.5px solid #021526"
+                                : errors.telNum
+                                ? "1.5px solid #F93B1D"
+                                : "1.5px solid green"
+                              : "none",
+                          padding: "2px",
+                          borderRadius: "6px",
+                          fontSize: "16px",
+                        }}
+                      />
+
+                      {telNumValue.length === 0 ? (
+                        <p style={{ color: "#021526" }}>
+                          ✓ მხოლოდ 9 რიცხვი დაიწყეთ 5 ით
+                        </p>
+                      ) : errors.telNum ? (
+                        <p style={{ color: "#F93B1D" }}>
+                          {errors.telNum.message}
+                        </p>
+                      ) : (
+                        <p style={{ color: "green" }}>
+                          ✓ მხოლოდ 9 რიცხვი დაიწყეთ 5 ით
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                  <div className="photos">
+                    <label
+                      htmlFor="file-upload"
+                      style={{ color: imageUrl ? "green" : "initial" }}
+                    >
+                      ატვირთეთ ფოტო *
+                    </label>
+                    {!imageUrl ? (
+                      <label
+                        className="custom-file-upload"
+                        htmlFor="file-upload"
+                      >
+                        <span className="plus">+</span>
+                        <input
+                          id="file-upload"
+                          type="file"
+                          accept="image/*"
+                          style={{ display: "none" }}
+                          onChange={handleFileChange}
+                        />
+                      </label>
+                    ) : (
+                      <div
+                        className="custom-file-upload"
+                        style={{
+                          border: imageUrl
+                            ? "2px dashed green"
+                            : "2px dashed #021526",
+                        }}
+                      >
+                        <div className="image-preview-wrapper">
+                          <img
+                            src={imageUrl}
+                            alt="preview"
+                            className="image-preview"
+                          />
+                          <button
+                            type="button"
+                            className="remove-image-btn"
+                            onClick={handleRemoveImage}
+                          >
+                            <img src={trash} alt="trash" className="trash" />
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="agent-btns">
+                    <button
+                      className="agent-remove-btn in-agent"
+                      onClick={remove}
+                    >
+                      გაუქმება
+                    </button>
+                    <button className="agent-add-btn in-agent">
+                      დაამატე აგენტი
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </form>
+        <div className="list-btns">
+          <Link to="/Main">
+            <button className="cancel btn">გაუქმება</button>
+          </Link>
+          <button className="add btn">დაამატე ლისტინგი</button>
+        </div>
       </div>
     </>
   );
